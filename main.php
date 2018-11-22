91fbd27d6cbf053bf01d684fd72b51d8bd2fb63c
P-a-n-d-a-0 <sunteregger14@gmail.com>
2018-11-22 09:44:13 +0100

    CSS menu bar in main.php

--- main.php
+++ main.php
@@ -18,45 +18,186 @@ include("PHP/MysqlStatement.class.php");
 
     <?php
 
-$Mysql = new Mysql();
+        $Mysql = new Mysql();
 
 
-$sql = "SELECT * FROM users WHERE name=:0 AND pw=:1";
-$sqlAufgaben = "SELECT * FROM aufgaben";
-$MysqlStatement_select = $Mysql->getMysqlStatement($sql);
-$MysqlStatement_select2 = $Mysql->getMysqlStatement($sqlAufgaben);
-$MysqlStatement_select->execute($_POST[name], $_POST[pw]); 
-$MysqlStatement_select2->execute($_POST[name], $_POST[pw]); 
+        $sql = "SELECT * FROM users WHERE name=:0 AND pw=:1";
+        $sqlAufgaben = "SELECT * FROM aufgaben";
+        $MysqlStatement_select = $Mysql->getMysqlStatement($sql);
+        $MysqlStatement_select2 = $Mysql->getMysqlStatement($sqlAufgaben);
+        $MysqlStatement_select->execute($_POST[name], $_POST[pw]); 
+        $MysqlStatement_select2->execute($_POST[name], $_POST[pw]); 
+    ?>
+
+    <div data-role="page" id="pageone">
+        <div data-role="header">
+            <h1>Aufgaben</h1>
+        </div>
+
+    
+
+    <div id="content">
+    <div data-role="main" class="ui-content">
+        <div data-role="tabs" id="tabs">
+            <div data-role="navbar">
+                <ul>
+                    <li><a href="#one" class="ui-btn-active" data-ajax="false">Upload</a></li>
+                    <li><a href="#two" data-ajax="false">Aufgaben</a></li>
+                    <li><a href="#three" data-ajax="false">History</a></li>
+                </ul>
+            </div>
+            <div id="one" class="tab-content ui-content" date-role="content">
+                <div class="ui-nodisc-icon ui-noback-icon">
+
+                    <a ref="#pagetwo" data-role="button" data-icon="camera" data-iconpos="notext" data-inline="true"></a>
+
+                    <div><a href="#pagetwo" data-role="button" data-icon="arrow-u" data-iconpos="left" data-inline="true" id="upload-button">Upload</a></div>
+                </div>
+            </div>
+            <div id="two" class="tab-content ui-content" date-role="content">
+            <?php echo "<br /> SQL Statement: <br/>" . $MysqlStatement_select->sql; ?>
+
+<?php echo "<br /> NUM: " . $MysqlStatement_select->num_rows; ?>
+<?php echo "<br /><br />"  ?>
+
+<?php
+
+    while ($data = $MysqlStatement_select2->fetchArray()) {
+    echo"<a href='givefb.php'>";
+
+    echo $data['a_name'];
+    echo"</a>";
+    echo "<br/>";}
 ?>
-
-<div data-role="page" id="pageone">
-    <div data-role="header">
-        <h1>Aufgaben</h1>
-
+                <ul data-role="listview" data-inset="true">
+                    <li>
+                        <a href="#popupPeter" data-rel="popup" data-position-to="window" data-transition="pop">
+                            <div>
+                                <div>
+                                    <img src="img/p3.jpg">
+                                </div>
+                                <p><strong>Peter</strong><br></p>
+                                <p>Which program for illustrating do<br>you recommend?</p>
+                            </div>
+                        </a>
+                        <div data-role="popup" id="popupPeter" data-overlay-theme="b" data-theme="a" class="ui-corner-all">
+                            <h1>Peter</h1>
+                            <a ref="#pagetwo" data-role="button" data-icon="comment" data-iconpos="notext" data-inline="true"></a>
+                            <img src="img/p3.jpg">
+                            <p>Which program for illustrating do<br>you recommend?</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#popupSara" data-rel="popup" data-position-to="window" data-transition="pop">
+                            <div>
+                                <div>
+                                    <img src="img/p1.jpg">
+                                </div>
+                                <p><strong>Sara</strong><br></p>
+                                <p>New Moodboard for my company!<br> What do you think?</p>
+                            </div>
+                        </a>
+                        <div data-role="popup" id="popupSara" data-overlay-theme="b" data-theme="a" class="ui-corner-all">
+                            <h1>Sara</h1>
+                            <a ref="#pagetwo" data-role="button" data-icon="comment" data-iconpos="notext" data-inline="true"></a>
+                            <img src="img/p1.jpg">
+                            <p>New Moodboard for my company!<br> What do you think?</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#popupSusi" data-rel="popup" data-position-to="window" data-transition="pop">
+                            <div>
+                                <div>
+                                    <img src="img/p2.jpg">
+                                </div>
+                                <p><strong>Susi</strong><br></p>
+                                <p>Don't know what to choose..<br> Please help!</p>
+                            </div>
+                        </a>
+                        <div data-role="popup" id="popupSusi" data-overlay-theme="b" data-theme="a" class="ui-corner-all">
+                            <h1>Susi</h1>
+                            <a ref="#pagetwo" data-role="button" data-icon="comment" data-iconpos="notext" data-inline="true"></a>
+                            <img src="img/p2.jpg">
+                            <p>Don't know what to choose..<br> Please help!</p>
+                        </div>
+                    </li>
+                    <!--?php
+                        for($i = 0; $i < 20; $i++) {
+                            echo   '<li><a href="#">
+                                        <div>
+                                            <div>
+                                                <img src="img/p1.jpg">
+                                            </div>
+                                            <p>Hello user '.($i+1).'</p>
+                                        </div>
+                                    </a></li>'
+                                    ;
+                        }
+                    ?-->
+                </ul>
+            </div>
+            
+            <div id="three" class="tab-content ui-content" date-role="content">
+                <ul data-role="listview" data-inset="true">
+                    <li>
+                        <a href="#popupPeter" data-rel="popup" data-position-to="window" data-transition="pop">
+                            <div>
+                                <div>
+                                    <img src="img/p3.jpg">
+                                </div>
+                                <p><strong>Peter</strong><br></p>
+                                <p>Which program for illustrating do<br>you recommend?</p>
+                            </div>
+                        </a>
+                        <div data-role="popup" id="popupPeter" data-overlay-theme="b" data-theme="a" class="ui-corner-all">
+                            <h1>Peter</h1>
+                            <a ref="#pagetwo" data-role="button" data-icon="comment" data-iconpos="notext" data-inline="true"></a>
+                            <img src="img/p3.jpg">
+                            <p>Which program for illustrating do<br>you recommend?</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#popupSara" data-rel="popup" data-position-to="window" data-transition="pop">
+                            <div>
+                                <div>
+                                    <img src="img/p1.jpg">
+                                </div>
+                                <p><strong>Sara</strong><br></p>
+                                <p>New Moodboard for my company!<br> What do you think?</p>
+                            </div>
+                        </a>
+                        <div data-role="popup" id="popupSara" data-overlay-theme="b" data-theme="a" class="ui-corner-all">
+                            <h1>Sara</h1>
+                            <a ref="#pagetwo" data-role="button" data-icon="comment" data-iconpos="notext" data-inline="true"></a>
+                            <img src="img/p1.jpg">
+                            <p>New Moodboard for my company!<br> What do you think?</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#popupSusi" data-rel="popup" data-position-to="window" data-transition="pop">
+                            <div>
+                                <div>
+                                    <img src="img/p2.jpg">
+                                </div>
+                                <p><strong>Susi</strong><br></p>
+                                <p>Don't know what to choose..<br> Please help!</p>
+                            </div>
+                        </a>
+                        <div data-role="popup" id="popupSusi" data-overlay-theme="b" data-theme="a" class="ui-corner-all">
+                            <h1>Susi</h1>
+                            <a ref="#pagetwo" data-role="button" data-icon="comment" data-iconpos="notext" data-inline="true"></a>
+                            <img src="img/p2.jpg">
+                            <p>Don't know what to choose..<br> Please help!</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 
 
-    <?php echo "<br /> SQL Statement: <br/>" . $MysqlStatement_select->sql; ?>
 
-    <?php echo "<br /> NUM: " . $MysqlStatement_select->num_rows; ?>
-    <?php echo "<br /><br />"  ?>
-
-
-    <?php
-   
-        while ($data = $MysqlStatement_select2->fetchArray()) {
-        echo"<a href='givefb.php'>";
-
-        echo $data['a_name'];
-        echo"</a>";
-        echo "<br/>";
-    }
-
-    ?>
-
-
-<div id="content">
-    <button onClick="printSomeThing();" class="buttonBlack">neue Aufgabe</button>
+        <button onClick="printSomeThing();" class="buttonBlack">neue Aufgabe</button>
     </div>
 
 </body>
