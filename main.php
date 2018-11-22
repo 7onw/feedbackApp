824c7c106d051e06bb38f0af9cc3bd611354af1f
P-a-n-d-a-0 <sunteregger14@gmail.com>
2018-11-22 08:56:55 +0100

    Titel geändert

--- main.php
+++ main.php
@@ -11,7 +11,7 @@ include("PHP/MysqlStatement.class.php");
     <link rel="stylesheet" type="text/css" href="CSS/style.css" media="only screen and (max-width:480px)">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <script language="javascript" type="text/javascript" src="JS/function.js"></script>
-    <title>FeedbackApp</title>
+    <title>Feedback-App</title>
 </head>
 <body>
 <body>
